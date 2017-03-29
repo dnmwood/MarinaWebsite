32566cefe70721d1e99c258ab6b3011071707f11
DAVID <davidnmwood@gmail.com>
2017-03-28 22:35:34 -0400

    set root page in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'website/index'
 
-  get 'website/contact'
+  root 'website#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
