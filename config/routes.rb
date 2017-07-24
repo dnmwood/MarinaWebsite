6e6a1d6bc288bb23de6ee199834326988b3854a5
DAVID <davidnmwood@gmail.com>
2017-07-24 15:54:20 -0400

    did a lot of stuff, added mail_form simple_form and created contact model, controller, views, migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+
+  get 'contacts/create'
 
   root 'website#index'
 
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
